59f91c6ff62eb40f4eb821f8cf6e5982ed29dd9b
Caleb Rocco Maccarone <maccarone1989@gmail.com>
2018-02-19 11:03:56 -0800

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Caleb Maccarone on 2/19/18.
+//  Copyright © 2018 Caleb Maccarone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
