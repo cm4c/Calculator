900567833788f0533fa1349da2fae3db75cc2fda
Korben Allen Rusek <korben.rusek@gmail.com>
2018-06-17 09:11:25 -0700

    refactor

--- Calculator/Controller/ViewController.swift
+++ Calculator/Controller/ViewController.swift
@@ -9,12 +9,7 @@
 import UIKit
 
 class ViewController : UIViewController {
-    var numbersHaveBeenTyped : Bool = false
-    var decimalHasBeenAdded = false
-    var num1Storage : Double = 0
-    var num2Storage : Double = 0
-    var operatorStorage : String = ""
-    var operation : String = ""
+    let calculator = Calculator()
     @IBOutlet weak var display: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,86 +18,25 @@ class ViewController : UIViewController {
     
     @IBAction func buttons(_ sender: UIButton) {
         let digit = sender.currentTitle!
-        
-        if numbersHaveBeenTyped {
-            let currentDisplay = display.text!
-            switch digit {
-            case "1","2","3","4","5","6","7","8","9":
-                if currentDisplay == "0" {
-                    display.text = digit
-                } else {
-                    display.text = currentDisplay + digit
-                }
-            case ".":
-                if !currentDisplay.hasSuffix(".") && !decimalHasBeenAdded {
-                    display.text = currentDisplay + digit
-                    decimalHasBeenAdded = true
-                }
-            case "0":
-                if currentDisplay != "0" {
-                    display.text = currentDisplay + digit
-                }
-            default:
-                display.text = "Error"
-            }
-        }else {
-            display.text = digit
-        }
-        numbersHaveBeenTyped = true
+        self.calculator.inputDigit(digit)
+        updateDisplay()
     }
+    
     @IBAction func operations(_ sender: UIButton) {
-        decimalHasBeenAdded = false
-        numbersHaveBeenTyped = false
-        operation = sender.currentTitle!
-        
-        operatorStorage = operation
-        switch operation {
-        case "C":
-            display.text = ""
-            num1Storage = 0
-            decimalHasBeenAdded = false
-        case "%":
-            display.text = String(Double(display.text!)!/100)
-        case "÷":
-            rememberNumbers()
-        case "X":
-            rememberNumbers()
-        case "-":
-            rememberNumbers()
-        case "+":
-            rememberNumbers()
-        default:
-            display.text = "error"}
+        guard let operation = Operator(rawValue: sender.currentTitle ?? "") else {
+            return
+        }
+        self.calculator.operate(operation)
+        updateDisplay()
     }
-    func rememberNumbers() {
-        num1Storage = Double(display.text!)!
-     numbersHaveBeenTyped = false
-    }
+    
     @IBAction func equals(_ sender: UIButton) {
-        let negAndEquals = sender.currentTitle!
-        switch negAndEquals {
-        case "+/-":
-            display.text = String(-Double(display.text!)!)
-        case "=":
-            num2Storage = Double(display.text!)!
-            switch operatorStorage {
-            case "÷":
-               display.text! = String(num1Storage/num2Storage)
-            case "X":
-                display.text! = String(num1Storage*num2Storage)
-            case "-":
-                display.text! = String(num1Storage-num2Storage)
-            case "+":
-                display.text! = String(num1Storage+num2Storage)
-            default:
-                display.text = "error"}
-                
-        default:
-             display.text = "error"
-        }
-        
-        }
+        self.calculator.evaluate()
+        updateDisplay()
     }
 
+    func updateDisplay() {
+        self.display.text = self.calculator.display
+    }
 
-
+}
